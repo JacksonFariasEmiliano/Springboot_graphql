3f7be25b02a67aea791f2a5aedbc61d9b52c9d91
Jackson Alves <jackson.emiliano15@gmail.com>
2022-04-29 23:36:52 -0300

    query graphql

---
+++ src/main/java/com/example/compras/Cliente.java
@@ -0,0 +1,16 @@
+package com.example.compras;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Cliente {
+
+    private String nome;
+    private String email;
+}
---
+++ src/main/java/com/example/compras/QueryGraphQl.java
@@ -0,0 +1,31 @@
+package com.example.compras;
+
+import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class QueryGraphQl implements GraphQLQueryResolver {
+
+    public String hello(){
+        return "Hello GraphQL";
+    }
+
+    public int soma(int a, int b){
+        return a+b;
+    }
+
+    public Cliente cliente(){
+        return new Cliente("Jackson", "jk@gmail.com");
+    }
+
+    public List<Cliente> clientes(){
+        List<Cliente> list = new ArrayList<>();
+        for (int i = 0; i < 10; i++){
+            list.add(new Cliente("Cliente " + i, "jk"+i+"@gmail.com"));
+        }
+        return list;
+    }
+}
